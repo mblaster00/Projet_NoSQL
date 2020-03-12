5de7a5168ed5e9e3e3d5ac761b38d4e8bef8fdf7
POD <papaomardiop00@gmail.com>
2020-03-12 21:19:30 +0000

    'debug'

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-var Twit = require(' t wit');
+var Twit = require('twit');
 config = require('./config.js');
-var
-T = new Twit(config);
-params = { q: 'neo4j ', count:lOO, lang:"en", since_id:620248636082024400 };
+var T = new Twit(config);
+//var T = new Twit(config.twitter);
+params = { q: 'neo4j', count:100, lang:"en", since_id:620248636082024400 };
 T.get('search/tweets', params, function(err, data, response) {
 console.log(data);
-} ) ;
+});
--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-module.experts = {
-    consumer_key : 'vePQ9jW27tB6tLpgNtRwWsEqH'
-    , consumer_secret : '41TTBRS2WsGssxU1gLmArGdD5HkhLBWXfFcAt5wwIkqFRNVbot'
-    , access_token : '899042534370009088-5R2xz8K9Iw9MLuWhf0KvCPF1tsYHJ3C'
-    ,access_token_secret: 'sNraanGKK56wFYvcyg0PFnxJzFcpyjsobELl9Q4CewbCO'
-    }
+module.exports = {
+    consumer_key : 'vePQ9jW27tB6tLpgNtRwWsEqH',
+    consumer_secret : '41TTBRS2WsGssxU1gLmArGdD5HkhLBWXfFcAt5wwIkqFRNVbot',
+    access_token : '899042534370009088-5R2xz8K9Iw9MLuWhf0KvCPF1tsYHJ3C',
+    access_token_secret: 'sNraanGKK56wFYvcyg0PFnxJzFcpyjsobELl9Q4CewbCO',
+}
