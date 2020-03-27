48b90b992fc6217d57959949d932307ad7c17147
POD <papaomardiop00@gmail.com>
2020-03-27 19:17:13 +0000

    'Nodejs'

--- Nodejs/app.js
+++
@@ -1,8 +0,0 @@
-var Twit = require('twit');
-config = require('./config.js');
-var T = new Twit(config);
-//var T = new Twit(config.twitter);
-params = { q: 'neo4j', count:100, lang:"en", since_id:620248636082024400 };
-T.get('search/tweets', params, function(err, data, response) {
-console.log(data);
-});
